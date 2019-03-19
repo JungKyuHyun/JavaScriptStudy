2627aa07749dbfb48eeba2ffe25c959f7ee0f0e3
Master Jung <ajdkfl6445@gmail.com>
2019-03-20 02:04:02 +0900

    2019-03-20 자바스크립트 공부

---
+++ vscode/test1.js
@@ -0,0 +1,14 @@
+
+var num1 = 15;
+var num2 = 2;
+var result;
+result = num1 + num2;
+documnet.write(result, "<br>");
+result = num1 - num2;
+documnet.write(result, "<br>");
+result = num1 * num2;
+documnet.write(result, "<br>");
+result = num1 / num2;
+documnet.write(result, "<br>");
+result = num1 % num2;
+documnet.write(result, "<br>");
